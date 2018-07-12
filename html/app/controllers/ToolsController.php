556cf3b142730460ed121f43f339cb76ddc6ad1c
Kai <kai@pazdzewicz.de>
2018-07-13 01:40:13 +0200

    created tools controller

---
+++ html/app/controllers/ToolsController.php
@@ -0,0 +1,27 @@
+<?php
+namespace Vokuro\Controllers;
+
+/**
+ * Display the default index page.
+ */
+class ToolsController extends ControllerBase
+{
+
+    /**
+     * Default action. Set the public layout (layouts/public.volt)
+     */
+    public function initialize()
+    {
+        $this->view->setTemplateBefore('public');
+    }
+
+    public function indexAction()
+    {
+
+    }
+
+    public function certinfoAction()
+    {
+
+    }
+}
