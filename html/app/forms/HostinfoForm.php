7ac52ae406a50c994a9f23f4dc16c3fb4284d880
donald <donald@fastnameserver.eu>
2018-07-17 08:13:25 +0200

    added hostinfo to tools controller

---
+++ html/app/forms/HostinfoForm.php
@@ -0,0 +1,22 @@
+<?php
+namespace Vokuro\Forms;
+use Phalcon\Forms\Form;
+use Phalcon\Forms\Element\Text;
+use Phalcon\Validation\Validator\PresenceOf;
+
+class HostinfoForm extends Form
+{
+  public function initialize($entity = null, $options = null)
+  {
+    $host = new Text('domain', [
+      'placeholder' => 'pki.server.pep',
+      'class' => 'form-control'
+    ]);
+    $host->addValidators([
+      new PresenceOf([
+        'message' => 'The domain is required'
+      ])
+    ]);
+    $this->add($host);
+  }
+}
