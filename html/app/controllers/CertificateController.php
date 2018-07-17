c93c42219e3f6f05bb6028a75bc24d6e0754ac56
Kai <kai@pazdzewicz.de>
2018-07-17 22:10:05 +0200

    made minor improvements

--- html/app/controllers/CertificateController.php
+++ html/app/controllers/CertificateController.php
@@ -1,5 +1,7 @@
 <?php
 namespace Vokuro\Controllers;
+use Phalcon\Paginator\Adapter\Model as Paginator;
+use Vokuro\Models\Certificates;
 
 
 /**
@@ -16,7 +18,20 @@ class CertificateController extends ControllerBase
     // List all Certificates
     public function indexAction()
     {
+      $identity = $this->auth->getIdentity();
 
+      $certs = Certificates::find("public = '1' OR userid = '".$identity["id"]."'");
+      if (count($users) == 0) {
+          $this->flash->notice("The search did not find any users");
+      }
+
+      $paginator = new Paginator([
+          "data" => $certs,
+          "limit" => 10,
+          "page" => $numberPage
+      ]);
+      var_dump($certs);
+      $this->view->page = $paginator->getPaginate();
     }
 
     // Create new SSL Certificate
---
+++ html/app/models/Certificates.php
@@ -0,0 +1,20 @@
+<?php
+namespace Vokuro\Models;
+use Phalcon\Mvc\Model;
+
+/**
+ * Certificates
+ */
+class Certificates extends Model
+{
+  public $serial_number;
+  public $authority_key_identifier;
+  public $ca_label;
+  public $status;
+  public $reason;
+  public $expiry;
+  public $revoked_at;
+  public $pem;
+  public $userid;
+  public $public;
+}
