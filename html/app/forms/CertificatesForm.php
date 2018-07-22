05ed34aabdfb9067449cab1107f6e00b5483266d
Kai Pazdzewicz <kai.pazdzewicz@adacor.com>
2018-07-22 22:33:02 +0200

    formatted a bit of code

--- html/app/forms/CertificatesForm.php
+++ html/app/forms/CertificatesForm.php
@@ -8,6 +8,20 @@ class CertificatesForm extends Form
 {
   public function initialize($entity = null, $options = null)
   {
+    $serial_number = new Text('serial_number', [
+      'placeholder' => '',
+      'aria-describedby' => 'name-addon',
+      'class' => 'form-control'
+    ]);
+
+    $serial_number->addValidators([
+      new PresenceOf([
+        'message' => 'The ca_label is required'
+      ])
+    ]);
+
+    $this->add($serial_number);
+
     if($options["edit"]==true)
     {
       $ca_label = new Text('ca_label', [
