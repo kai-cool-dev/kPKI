3062760b350227cb8e2cd1da3300070c7b15480d
Kai Pazdzewicz <kai.pazdzewicz@adacor.com>
2018-07-26 14:44:01 +0200

    removed some unused files

--- start_ocsp_service.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/pki/client/cfssl ocspserve -port=8889 -responses=/pki/ocspdump.txt  -loglevel=0
--- start_pki_service.sh
+++
@@ -1,1 +0,0 @@
-/pki/client/cfssl serve -db-config=/pki/conf/config_mysql.json -loglevel=0 -ca-key=/pki/certs/intermediate/intermediate.key.pem -ca=/pki/certs/intermediate/intermediate.crt.pem -config=/pki/conf/config_ca.json -responder=/pki/ocsp/server-ocsp.pem -responder-key=/pki/ocsp/server-ocsp-key.pem
